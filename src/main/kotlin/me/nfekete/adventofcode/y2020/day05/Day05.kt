fc8666f25ec96358f37ceaa3327be7202aac8c4d
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-05 21:49:34 +0100

    Day05: Create a nicer translation to binary

--- src/main/kotlin/me/nfekete/adventofcode/y2020/day05/Day05.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day05/Day05.kt
@@ -6,20 +6,20 @@ object Day05 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val seatIds = classpathFile("input.txt").readLines()
-            .map { it.translateToBinary() }
-            .map { it.toInt(2) }
-        val maxSeatNr = seatIds
-            .maxOrNull()!!
+        val seatIds = classpathFile("input.txt").readLines().map { it.translateToBinary() }
+        val minSeatNr = seatIds.minOrNull()!!
+        val maxSeatNr = seatIds.maxOrNull()!!
         println("Max seat index: $maxSeatNr")
-        (1 until maxSeatNr).filter { it - 1 in seatIds && it + 1 in seatIds && it !in seatIds }
-            .forEach { println("Your seat index: $it") }
+        (minSeatNr until maxSeatNr).singleOrNull { it !in seatIds }
+            ?.let { println("Your seat index: $it") }
+            ?: println("No empty seats or multiple empty seats found")
     }
 
     private fun String.translateToBinary() =
-        replace('B', '1')
-            .replace('F', '0')
-            .replace('R', '1')
-            .replace('L', '0')
-
+        fold(0) { acc, char ->
+            when (char) {
+                'B', 'R' -> acc shl 1 or 1
+                else -> acc shl 1
+            }
+        }
 }
