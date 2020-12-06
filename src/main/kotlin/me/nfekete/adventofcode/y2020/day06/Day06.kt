f0f4d048c603c9aec7f8ec3898e466bc5032ea8f
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-06 14:34:28 +0100

    Day06 quick and dirty

---
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day06/Day06.kt
@@ -0,0 +1,42 @@
+package me.nfekete.adventofcode.y2020.day06
+
+import me.nfekete.adventofcode.y2020.common.classpathFile
+
+object Day06 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = classpathFile("input.txt").readLines().asSequence()
+            .chunkBy { it.isEmpty() }.toList()
+        val sumYes = input
+            .map { group ->
+                group.flatMap { personYesAnswers ->
+                    personYesAnswers.asSequence()
+                }.toCollection(HashSet())
+                    .size
+            }.sum()
+        println("Sum of YES answers from each group: $sumYes")
+        val sumAllYes = input
+            .map { group ->
+                val answerGroups = group.flatMap { personYesAnswers ->
+                    personYesAnswers.asSequence()
+                }.groupBy { it }
+                val countOfAnswersThatWereAllYes = answerGroups.values.filter { it.size == group.size }.count()
+                countOfAnswersThatWereAllYes
+            }.sum()
+        println("Sum of the count of answers that were all yes from each group: $sumAllYes")
+    }
+
+    private fun Sequence<String>.chunkBy(predicate: (String) -> Boolean) = sequence {
+        val currentChunk = mutableListOf<String>()
+        for (line in this@chunkBy) {
+            if (predicate(line)) {
+                yield(currentChunk.toList())
+                currentChunk.clear()
+            } else {
+                currentChunk.add(line)
+            }
+        }
+        yield(currentChunk.toList())
+    }
+}
