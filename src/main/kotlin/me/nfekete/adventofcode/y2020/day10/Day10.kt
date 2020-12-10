07a6e9388c4ee5596434052f40856a47c71980ed
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-10 13:42:06 +0100

    Day 10: improve initial setup

--- src/main/kotlin/me/nfekete/adventofcode/y2020/day10/Day10.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day10/Day10.kt
@@ -6,8 +6,8 @@ object Day10 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val input = classpathFile("input.txt").readLines().map { it.toInt() }
-        val extendedInput = (listOf(0) + input.sorted() + (3 + input.maxOrNull()!!))
+        val input = classpathFile("input.txt").readLines().map { it.toInt() }.sorted()
+        val extendedInput = (listOf(0) + input + (3 + input.last()))
         val deltas = extendedInput.windowed(2, 1, partialWindows = false).map { (a, b) -> b - a }
         (deltas.count { it == 1 } to deltas.count { it == 3 })
             .let { (a, b) -> println("Nr of 1-differences * Nr of 3 differences: $a * $b = ${a * b}") }
@@ -26,5 +26,5 @@ private fun List<Int>.numberOfArrangements(): Long {
         val c = if (index - 3 in set) dp[index - 3] else 0
         dp[index] = a + b + c
     }
-    return dp[max]
+    return dp.last()
 }
