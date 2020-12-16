eba52a87b0beb7a9f0b5b9bbcecbc85eb1ea8ee7
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-16 13:50:09 +0100

    Day 16 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2020/day16/Day16.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day16/Day16.kt
@@ -7,27 +7,50 @@ import me.nfekete.adventofcode.y2020.common.splitByDelimiter
 
 data class Rule(val name: String, val ranges: List<ClosedRange<Int>>)
 
-fun Rule.validate(value: Int) = ranges.any { range -> range.contains(value) }
-fun List<Rule>.validate(value: Int) = any { rule -> rule.validate(value) }
-
-fun parseRule(line: String) = line.splitByDelimiter(": ").map2 { ranges ->
+private fun Rule.validate(value: Int) = ranges.any { range -> range.contains(value) }
+private fun List<Rule>.validate(value: Int) = any { rule -> rule.validate(value) }
+private fun String.parseTicket() = split(",").map(String::toInt)
+private fun parseRule(line: String) = line.splitByDelimiter(": ").map2 { ranges ->
     ranges.split(" or ").map { range -> range.splitByDelimiter('-') }.map { (from, to) -> from.toInt()..to.toInt() }
 }.let { (name, ranges) -> Rule(name, ranges) }
 
-fun String.parseTicket() = split(",").map(String::toInt)
+private fun List<List<Int>>.column(column: Int) = map { it[column] }
 
 object Day16 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val it = classpathFile("sample.txt").readLines().asSequence().chunkBy { it.isBlank() }.iterator()
+        val it = classpathFile("input.txt").readLines().asSequence().chunkBy { it.isBlank() }.iterator()
         val rules = it.next().map(::parseRule)
         val myTicket = it.next().drop(1).first().parseTicket()
         val otherTickets = it.next().drop(1).map(String::parseTicket)
 
-        part1(rules, myTicket, otherTickets).let(::println)
+        part1(rules, otherTickets).let(::println)
+        part2(rules, myTicket, otherTickets).let(::println)
     }
 
-    private fun part1(rules: List<Rule>, myTicket: List<Int>, otherTickets: List<List<Int>>): Int =
+    private fun part1(rules: List<Rule>, otherTickets: List<List<Int>>): Int =
         otherTickets.flatten().filter { value -> !rules.validate(value) }.sum()
+
+    private fun part2(rules: List<Rule>, myTicket: List<Int>, otherTickets: List<List<Int>>): Long {
+        val validTickets = otherTickets.filter { values -> values.all { value -> rules.validate(value) } }
+
+        val columns = validTickets.first().indices
+        val rulesWithPossibleColumns = rules.map { rule ->
+            rule to columns.filter { columnIndex ->
+                val column = validTickets.column(columnIndex)
+                column.all { rule.validate(it) }
+            }
+        }.sortedBy { it.second.size }
+        val columnsWithMatchingRules = mutableSetOf<Int>()
+        val rulesToColumnsMap =
+            rulesWithPossibleColumns.map { (rule, columns) ->
+                val pair = rule to (columns.toSet() - columnsWithMatchingRules)
+                columnsWithMatchingRules.addAll(columns)
+                pair
+            }.map { it.map2 { set -> set.single() } }
+        val departureFields = rulesToColumnsMap.filter { (rule, _) -> rule.name.startsWith("departure") }
+        departureFields.forEach(::println)
+        return departureFields.map { (_, column) -> myTicket[column] }.fold(1L) { a, b -> a * b }
+    }
 }
