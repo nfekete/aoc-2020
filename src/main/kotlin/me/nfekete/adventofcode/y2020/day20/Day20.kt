e60d21aa1c0ddec5e892029cc74cd60e4b0001ec
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-21 15:23:51 +0100

    Cleanup of Day 20

--- src/main/kotlin/me/nfekete/adventofcode/y2020/day20/Day20.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day20/Day20.kt
@@ -4,31 +4,23 @@ import me.nfekete.adventofcode.y2020.common.*
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
-fun Int.flip(bits: Int = 10): Int {
-    val msbMask = 1 shl (bits - 1)
-    var orig = this
-    var result = 0
-    for (i in 1..bits) {
-        result = result shr 1 or (orig and msbMask)
-        orig = orig shl 1
-    }
-    return result
-}
-
 fun List<Boolean>.toInt() = fold(0) { acc, b -> acc shl 1 or if (b) 1 else 0 }
 fun List<List<Boolean>>.rotate() = indices.map { row ->
     indices.map { column -> this[size - 1 - column][row] }
 }
+
 fun List<List<Boolean>>.flipV() = indices.map { row ->
     indices.map { column -> this[row][size - 1 - column] }
 }
+
 fun List<List<Boolean>>.flipH() = indices.map { row ->
     indices.map { column -> this[size - 1 - row][column] }
 }
 
-typealias TileFunction = (Tile)->Tile
-fun TileFunction.compose(f: TileFunction) = { tile: Tile -> invoke(f(tile)) }
-fun identity(tile: Tile) = tile
+typealias TileFunction = (Tile) -> Tile
+
+private fun TileFunction.compose(f: TileFunction) = { tile: Tile -> invoke(f(tile)) }
+private fun identity(tile: Tile) = tile
 
 data class Sides(val top: Int, val right: Int, val bottom: Int, val left: Int) {
     val all get() = listOf(top, right, bottom, left)
@@ -47,8 +39,8 @@ data class Tile(val id: Long, val bitmap: List<List<Boolean>>) {
     val sides: Sides = extractSides(bitmap)
 
     fun orientations(): Set<Tile> {
-        val rotations = generateSequence<TileFunction>(::identity) { f -> { tile -> f(tile).rotate90cw()} }.take(4)
-        val flips = sequenceOf({tile -> tile}, Tile::flipV, Tile::flipH)
+        val rotations = generateSequence<TileFunction>(::identity) { it.compose(Tile::rotate90cw) }.take(4)
+        val flips = sequenceOf(::identity, Tile::flipV, Tile::flipH)
         return rotations.flatMap { f ->
             flips.map { g -> f.compose(g) }
         }.map { function -> function.invoke(this) }.toSet()
@@ -93,19 +85,16 @@ private fun Map<Int, Set<Tile>>.tilesWithNonSharedEdgePatterns(): Map<Tile, Int>
 private fun Map<Tile, Int>.filterToNonSharedCount(desiredCount: Int) =
     filter { (_, count) -> count == desiredCount }.map { (k, _) -> k }
 
-fun findCornerAndBorderTiles(tiles: List<Tile>): Pair<List<Tile>, List<Tile>> {
-    val size = tiles.mapSize().also { println("Map size: ${it}*${it}") }
+fun findCornerTiles(tiles: List<Tile>): List<Tile> {
     val mapBySide = tiles.sidePatternSetToTileMap()
     // sides that belong to a single tile - these must belong to tiles that are on the edge
     val tilesWithCounts = mapBySide.tilesWithNonSharedEdgePatterns()
-    val sides = tilesWithCounts.filterToNonSharedCount(2).windowed((size - 2) * 4).single() //assert the size
-    val corners = tilesWithCounts.filterToNonSharedCount(4).windowed(4).single() //assert the size
-    return corners to (corners + sides)
+    return tilesWithCounts.filterToNonSharedCount(4).windowed(4).single() //assert the size
 }
 
 private fun Collection<Tile>.mapSize() = sqrt(count().toDouble()).roundToInt()
 
-private fun fillMap(corners: Collection<Tile>, tiles: Collection<Tile>): Sequence<List<Tile>> {
+private fun validTileArrangements(corners: Collection<Tile>, tiles: Collection<Tile>): Sequence<List<Tile>> {
     val size = tiles.mapSize()
     val range = 0 until size
 
@@ -126,23 +115,23 @@ private fun fillMap(corners: Collection<Tile>, tiles: Collection<Tile>): Sequenc
         }
         return candidates.asSequence().flatMap { tryFill(map + it, set - it.orientations(), index + 1) }
     }
-
-    val allFills = tryFill(listOf(), tiles.toSet(), 0)
-//    allFills.map { list ->
-//        listOf(
-//            list[0],
-//            list[tiles.mapSize()-1],
-//            list[tiles.mapSize() * (tiles.mapSize()-1)],
-//            list[tiles.mapSize() * tiles.mapSize() - 1],
-//        )
-//    }.map { list -> list.map { it.id } }.forEach(::println)
-    return allFills
+    return tryFill(listOf(), tiles.toSet(), 0)
 }
 
 data class Bitmap(val bits: List<List<Boolean>>) {
     val onBitsCount get() = bits.flatten().count { it }
     val size get() = bits.size
     fun isSet(row: Int, column: Int) = bits[row][column]
+    fun findMonster(monster: Monster): List<Pair<Int, Int>> {
+        val (height, width) = monster.mask.run { maxOfOrNull { it.first }!! + 1 to maxOfOrNull { it.second }!! + 1 }
+        return (0..size - height).flatMap { row ->
+            (0..size - width).mapNotNull { column ->
+                val found = monster.mask.all { (drow, dcolumn) -> isSet(row + drow, column + dcolumn) }
+                if (found) row to column else null
+            }
+        }
+    }
+
     override fun toString(): String =
         bits.joinToString("\n") { row -> row.map { if (it) '#' else '.' }.joinToString("") }
 }
@@ -157,36 +146,15 @@ private fun List<Tile>.toBitmap(): Bitmap {
         val tileIndex = tileRow * mapSize + tileColumn
         return get(tileIndex).bitmap[1 + rowOffsetWithinTile][1 + columnOffsetWithinTile]
     }
+
     val size = mapSize() * 8
     return (0 until size).map { row ->
-        (0 until size).map { column ->
-            isSet(row, column)
-        }
+        (0 until size).map { column -> isSet(row, column) }
     }.let(::Bitmap)
 }
 
-typealias CoordinateFunction = (Int, Int) -> Pair<Int, Int>
-fun CoordinateFunction.compose(f: CoordinateFunction) =
-    { row: Int, column: Int -> invoke(row, column).let { (row, column) -> f(row, column) } }
-fun identity(row: Int, column: Int): Pair<Int, Int> = row to column
-fun rotate90cw(size: Int): CoordinateFunction = { row: Int, column: Int -> (size - 1) - column to row }
-fun flipV(size: Int): CoordinateFunction = { row: Int, column: Int -> row to (size - 1) - column }
-fun flipH(size: Int): CoordinateFunction = { row: Int, column: Int -> (size - 1) - row to column }
-data class BitMapAddressing(val bitmap: Bitmap, val coordinateFunction: CoordinateFunction) {
-    val onBitsCount get() = bitmap.onBitsCount
-
-    private fun isSet(row: Int, column: Int) = coordinateFunction(row, column).let { (row, column) -> bitmap.isSet(row, column) }
-    fun findMonster(monster: Monster): List<Pair<Int, Int>> {
-        val (height, width) = monster.mask.run { maxOfOrNull { it.first }!!+1 to maxOfOrNull { it.second }!!+1 }
-        return (0..bitmap.size - height).flatMap { row ->
-            (0..bitmap.size - width).mapNotNull { column ->
-                val found = monster.mask.all { (drow, dcolumn) -> isSet(row + drow, column + dcolumn) }
-                if (found) row to column else null
-            }
-        }
-    }
-}
 data class Monster(val mask: List<Pair<Int, Int>>)
+
 private fun List<String>.toMonster(): Monster = flatMapIndexed { row, line ->
     line.mapIndexedNotNull { column, char -> if (char == '#') row to column else null }
 }.let(::Monster)
@@ -196,29 +164,16 @@ object Day20 {
     @JvmStatic
     fun main(args: Array<String>) {
         val tiles = classpathFile("input.txt").lineSequence().chunkBy { it.isBlank() }.map(Tile::from).toList()
-        val (corners, _) = findCornerAndBorderTiles(tiles)
-        corners.map { it.id }.onEach(::println).product().also { println("Product of corner ids: $it") }
+        findCornerTiles(tiles).map { it.id }.onEach(::println).product().also { println("Product of corner ids: $it") }
 
         val monster = classpathFile("monster.txt").readLines().toMonster()
-        val bitmaps = fillMap(corners, tiles).map { it.toBitmap() }
-//        val rotations = generateSequence<CoordinateFunction>(::identity) { fn -> fn.compose(rotate90cw(bitmaps.first().size)) }.take(4)
-//        val flips = bitmaps.first().size.let { size ->
-//            sequenceOf(::identity, flipH(size), flipV(size))
-//        }
-//        val addressings = bitmaps.flatMap { bitmap ->
-//            crossProduct(rotations, flips) { f, g -> f.compose(g) }.map { transformation ->
-//                BitMapAddressing(bitmap, transformation)
-//            }
-//        }.asSequence()
-        val addressings = bitmaps.map {
-            BitMapAddressing(it) { a, b -> a to b }
-        }
-        addressings.map { it to it.findMonster(monster) }.filter { it.second.isNotEmpty() }
+        val bitmaps = validTileArrangements(findCornerTiles(tiles), tiles).map { it.toBitmap() }
+        bitmaps.map { it to it.findMonster(monster) }
+            .filter { it.second.isNotEmpty() }
             .map { it.map2 { list -> list.count() } }
-            .onEach { println("${it.first.bitmap}\n${it.second}\n") }
+            .onEach { println("${it.first}\n${it.second}\n") }
             .first().let {
                 println(it.first.onBitsCount - it.second * monster.mask.size)
             }
     }
 }
-
--- src/test/kotlin/me/nfekete/adventofcode/y2020/day20/Day20KtTest.kt
+++ src/test/kotlin/me/nfekete/adventofcode/y2020/day20/Day20KtTest.kt
@@ -4,15 +4,6 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 internal class Day20KtTest: FunSpec({
-    test("Int.flip should flip bit order") {
-        1.flip(2) shouldBe 2
-        2.flip(2) shouldBe 1
-        3.flip(8) shouldBe 192
-        192.flip(8) shouldBe 3
-        0.flip(10) shouldBe 0
-        1.flip(10) shouldBe 512
-        512.flip(10) shouldBe 1
-    }
     test("List<Boolean>.toInt() should convert Boolean array to Int") {
         listOf(false, false).toInt() shouldBe 0
         listOf(false, true).toInt() shouldBe 1
