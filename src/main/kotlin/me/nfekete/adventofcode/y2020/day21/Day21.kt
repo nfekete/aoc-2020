ebe698996b125d53b64ef43402b5d7117a6e6937
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-21 17:23:02 +0100

    Day 21 part 2

--- src/main/kotlin/me/nfekete/adventofcode/y2020/day21/Day21.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day21/Day21.kt
@@ -10,22 +10,26 @@ object Day21 {
             line.splitByDelimiter(" (contains ")
                 .map1 { it.split(" ") }
                 .map2 { it.removeSuffix(")").split(", ") }
-        }.onEach(::println)
+        }
         val allIngredients = input.flatMap { it.first }.toSet()
-        val map = input.map { it.swapped }
+        val mapAllergenToSetsOfIngredients = input.map { it.swapped }
             .flatMap { it.first.map { allergen -> allergen to it.second.toSet() } }
             .groupBy { it.first }
             .mapValues { (_, value) -> value.unzip().second }
         val narrowSetOfIngredientsByAllergens =
-            map.mapValues { (_, ingredientSets) -> ingredientSets.reduce { acc, set -> acc.intersect(set) } }
+            mapAllergenToSetsOfIngredients.mapValues { (_, ingredientSets) -> ingredientSets.reduce { acc, set -> acc.intersect(set) } }
         val ingredientsNotAssociatedWithAllergens = allIngredients - narrowSetOfIngredientsByAllergens.values.flatten()
         val numberOfTimesIngredientsNotAssociatedWithAllergensAppear =
             ingredientsNotAssociatedWithAllergens.map { ingredient -> input.count { (ingredients, _) -> ingredient in ingredients } }
                 .sum()
-        println("All ingredients: $allIngredients")
-        println("Ingredients to allergens map: $map")
-        println("Narrow set of ingredients by allergens: $narrowSetOfIngredientsByAllergens")
-        println("Ingredients that are not associated with any allergens: $ingredientsNotAssociatedWithAllergens")
+        val (_, resolved) = generateSequence(narrowSetOfIngredientsByAllergens to emptyMap<String, String>()) { (candidates, resolved) ->
+            val newlyResolved = candidates.filter { (_, v) -> v.size == 1 }.mapValues { (_, v) -> v.single() }
+            val resolved = resolved + newlyResolved
+            val candidates = (candidates - newlyResolved.keys).mapValues { (_, v) -> v - newlyResolved.values }
+            candidates to resolved
+        }.first { (ambiguous, _) -> ambiguous.isEmpty() }
+        val ingredientsSortedByAllergens = resolved.entries.sortedBy { it.key }.joinToString(",") { it.value }
         println("Number of times ingredients not associated with allergens appear: $numberOfTimesIngredientsNotAssociatedWithAllergensAppear")
+        println("Ingredients sorted by their allergen: $ingredientsSortedByAllergens")
     }
 }
