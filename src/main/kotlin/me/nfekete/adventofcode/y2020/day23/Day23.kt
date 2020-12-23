afb8de3767c71703755b64ae4f8e166efb9637a3
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-23 15:07:01 +0100

    Day 23 part 1 fixed

--- src/main/kotlin/me/nfekete/adventofcode/y2020/day23/Day23.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day23/Day23.kt
@@ -2,30 +2,30 @@ package me.nfekete.adventofcode.y2020.day23
 
 object Day23 {
 
-    val String.circular get() = this + this
-    fun String.shift(chars: Int) = circular.drop(chars).take(length)
-    fun String.insertAt(index: Int, insertion: String) = this.take(index) + insertion + this.drop(index)
+    private val String.circular get() = this + this
+    private fun String.shiftLeft(chars: Int) = circular.drop((length + chars) % length).take(length)
+    private fun String.shiftRight(chars: Int) = shiftLeft(-chars)
+    private fun String.insertAt(index: Int, insertion: String) = this.take(index) + insertion + this.drop(index)
 
-    fun rearrange(input: String, currentCupIndex: Int): Pair<String, Int> {
+    private fun rearrange(input: String, currentCupIndex: Int): Pair<String, Int> {
         val currentCupIndex = currentCupIndex % input.length
-        val takenOutCups = input.shift(currentCupIndex + 1).take(3).also { println("Picked: $it") }
-        val remainingCups = input.shift(currentCupIndex + 1).drop(3).also { println("Remaining: $it") }
-        val remainingCupLabels = remainingCups.map { "$it".toInt() }
-        val currentCupLabel = "${input.circular[currentCupIndex]}".toInt()
-        val nextCupLabel = '0' + generateSequence(currentCupLabel - 1) { label ->
-            if (remainingCupLabels.contains(label - 1)) label - 1 else remainingCupLabels.maxOrNull()!!
-        }.first { remainingCups.contains(('0' + it)) }
-        val destinationIndex = remainingCups.indexOf(nextCupLabel)
-        val reinserted = remainingCups.insertAt(destinationIndex + 1, takenOutCups)
-        val nextCupIndex = (reinserted.indexOf('0' + currentCupLabel) + 1) % input.length
-        return reinserted to nextCupIndex
+        val currentCup = input[currentCupIndex].also { println("Current cup: $it") }
+        val takenOutCups = input.shiftLeft(currentCupIndex + 1).take(3).also { println("Picked: $it") }
+        val remainingCups = input.shiftLeft(currentCupIndex + 1).drop(3)//.also { println("Remaining: $it") }
+        val destinationCupLabel = generateSequence(currentCup) { label ->
+            if (label > '1') label - 1 else remainingCups.maxOrNull()!!
+        }.drop(1).first { it in remainingCups }.also { println("Destination cup: $it") }
+        val destinationIndex = remainingCups.indexOf(destinationCupLabel)
+        val reinserted = takenOutCups + remainingCups.shiftLeft(destinationIndex + 1)
+        val nextCupIndex = (reinserted.indexOf(currentCup) + 1) % input.length
+        return reinserted.shiftLeft(nextCupIndex) to 0
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
         val sample = "389125467"
         val input = "389547612"
-        val steps = 10
+        val steps = 100
         val (cups, _) = generateSequence(sample to 0) { (input, currentCupIndex) ->
             rearrange(
                 input,
@@ -33,6 +33,6 @@ object Day23 {
             )
         }.onEach(::println)
             .take(1 + steps).last()
-        cups.shift(cups.indexOf('1')).drop(1).also(::println)
+        cups.shiftLeft(cups.indexOf('1')).drop(1).also(::println)
     }
 }
