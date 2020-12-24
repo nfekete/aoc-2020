b81f73a42fbff74f6520e13683c6b34d1c76390a
Nándor Előd Fekete <nandor.fekete@gmail.com>
2020-12-24 01:35:40 +0100

    Day 23 part 2 first attempt

--- src/main/kotlin/me/nfekete/adventofcode/y2020/day23/Day23.kt
+++ src/main/kotlin/me/nfekete/adventofcode/y2020/day23/Day23.kt
@@ -1,9 +1,11 @@
 package me.nfekete.adventofcode.y2020.day23
 
+import me.nfekete.adventofcode.y2020.common.product
+
 object Day23 {
 
-    private val String.circular get() = this + this
-    private fun String.shiftLeft(chars: Int) = circular.drop((length + chars) % length).take(length)
+    private fun String.shiftLeft(chars: Int) =
+        ((length + chars) % length).let { this.drop(it).take(length) + this.take(it) }
 
     private fun rearrange(input: String): String {
         val currentCup = input.first().also { println("Current cup: $it") }
@@ -17,13 +19,87 @@ object Day23 {
         val nextCupIndex = (reinserted.indexOf(currentCup) + 1) % input.length
         return reinserted.shiftLeft(nextCupIndex)
     }
+    private val String.result get() = shiftLeft(indexOf('1')).drop(1)
+
+
+    class CyclicMutableList<T>(var shift: Int, val list: MutableList<T>): AbstractMutableList<T>() {
+        private fun address(index: Int) = (list.size + index + shift) % list.size
+        fun move3(fromIndex: Int, toIndex: Int) = this.apply {
+            val list = shift(fromIndex).take(3).also { shift(-fromIndex) }
+            if (fromIndex < toIndex) {
+                for (i in fromIndex until toIndex) {
+                    this[i] = this[i + 3]
+                }
+                for (i in toIndex until toIndex + 3) {
+                    this[i] = list[i-toIndex]
+                }
+            } else if (fromIndex > toIndex) {
+                for (i in toIndex until toIndex + 3) {
+                    this[i] = list[i-toIndex]
+                }
+                for (i in fromIndex + 3 downTo toIndex) {
+                    this[i] = this[i - 3]
+                }
+            }
+        }
+
+        override fun add(index: Int, element: T) = list.add(address(index), element)
+
+        override fun removeAt(index: Int): T = list.removeAt(address(index))
+
+        override fun set(index: Int, element: T): T = list.set(address(index), element)
+
+        override val size: Int get() = list.size
+
+        override fun get(index: Int): T = list[address(index)]
+
+        fun shift(shift: Int): CyclicMutableList<T> = this.apply {
+            this.shift += shift
+        }
+    }
+
+    private val CyclicMutableList<Int>.result
+        get() = shift(indexOf(1)).asSequence().drop(1).take(2).map(Int::toLong).toList().also(::println).product()
+
+
+    class Part2(input: String, val maxValue: Int = 1_000_000) {
+        private val extendedProblem = input.let {
+            val initialSequence = it.map(Char::toString).map(String::toInt)
+            val rest = initialSequence.maxOrNull()!! + 1..maxValue
+            CyclicMutableList(0, mutableListOf<Int>().apply {
+                addAll(initialSequence)
+                addAll(rest)
+            })
+        }
+
+        private fun rearrange(input: CyclicMutableList<Int>): CyclicMutableList<Int> {
+            val currentCup = input.first()
+            val takenOutCups = input.asSequence().drop(1).take(3).toList()
+            val destinationCupLabel = generateSequence(currentCup) { label ->
+                if (label > 1) label - 1 else maxValue
+            }.drop(1).first { it !in takenOutCups }
+            val destinationIndex = input.indexOf(destinationCupLabel) - 2
+            println("Destination index = $destinationIndex")
+            input.move3(1, destinationIndex)
+            input.shift(input.indexOf(currentCup) + 1)
+            println("Next first element = ${input.first()}")
+            return input
+        }
+
+        fun run(steps: Int = 10_000_000): CyclicMutableList<Int> = generateSequence(extendedProblem, ::rearrange).drop(steps).first()
+    }
 
     @JvmStatic
     fun main(args: Array<String>) {
         val sample = "389125467"
         val input = "389547612"
         val steps = 100
-        val cups = generateSequence(input, ::rearrange).onEach(::println).take(1 + steps).last()
-        cups.shiftLeft(cups.indexOf('1')).drop(1).also(::println)
+//        val cups = generateSequence(sample, ::rearrange).onEach(::println).drop(steps).first()
+
+        Part2(input).run(steps).also {
+//            println(it)
+            println("Part 2 result = ${it.result}")
+        }
     }
+
 }
